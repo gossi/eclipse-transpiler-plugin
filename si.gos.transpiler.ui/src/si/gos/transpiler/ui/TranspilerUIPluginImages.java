e059e2d3a5072e251299d9d92e9ba21221d2c07e
apptaro <apptaro@gmail.com>
2017-06-23 15:24:06 +0900

    Fix for Issue #19
    Make it work with Eclipse Neon

--- si.gos.transpiler.ui/src/si/gos/transpiler/ui/TranspilerUIPluginImages.java
+++ si.gos.transpiler.ui/src/si/gos/transpiler/ui/TranspilerUIPluginImages.java
@@ -5,6 +5,6 @@ import org.eclipse.wb.swt.ResourceManager;
 
 public class TranspilerUIPluginImages {
 
-	public static ImageDescriptor FOLDER = ResourceManager.getPluginImageDescriptor("org.eclipse.ui.ide", "/icons/full/obj16/folder.gif");
-	public static ImageDescriptor FILE = ResourceManager.getPluginImageDescriptor("org.eclipse.ui", "/icons/full/obj16/file_obj.gif");
+	public static ImageDescriptor FOLDER = ResourceManager.getPluginImageDescriptor("org.eclipse.ui.ide", "/icons/full/obj16/folder.png");
+	public static ImageDescriptor FILE = ResourceManager.getPluginImageDescriptor("org.eclipse.ui", "/icons/full/obj16/file_obj.png");
 }
